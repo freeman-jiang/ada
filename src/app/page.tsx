f70bbe4ce720cfbaf92a1ee025bc2ec94544e59f
Freeman Jiang <freeman.jiang.ca@gmail.com>
2024-12-20 16:03:17 +0100

    complete pie for languages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,25 @@ import { Markdown } from "@/components/Markdown";
 import { AllEmotions } from "@/components/charts/pie/AllEmotions";
 import { HorizontalBar } from "@/components/charts/bar/HorizontalBar";
 import { VerticalBar } from "@/components/charts/bar/VerticalBar";
+import { PieChartCard } from "@/components/charts/pie/Pie";
 
 export default async function Home() {
   const content = await getContent("README.md");
   const actorEmotionData = await getJSONContent("actor_emotion.json");
   const actorGenderData = await getJSONContent("actor_gender.json");
+  const languagesData = await getJSONContent("languages.json");
+
   return (
     <div className="bg-grid-white/[0.02] font-sans">
       <Hero />
       <div className="w-full h-px bg-gradient-to-r from-transparent via-zinc-50/20 to-transparent my-20" />
       <div className="w-full flex flex-col px-4 lg:px-[25vw] items-center">
         <div className="max-w-[65ch]">
+          <PieChartCard
+            data={languagesData}
+            title="Top 15 Movie Languages"
+            description="Some movies are in multiple languages."
+          />
           <AllEmotions />
           <HorizontalBar chart={actorEmotionData} />
           <VerticalBar chart={actorEmotionData} />
---
+++ src/components/charts/pie/Pie.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+interface PieChartProps {
+  data: Record<string, number>;
+  title: string;
+  description?: string;
+}
+
+const COLORS = [
+  "#2563eb",
+  "#dc2626",
+  "#16a34a",
+  "#9333ea",
+  "#ea580c",
+  "#0891b2",
+  "#4f46e5",
+  "#be123c",
+  "#15803d",
+  "#7c2d12",
+  "#86198f",
+  "#1e40af",
+  "#b91c1c",
+  "#115e59",
+  "#581c87",
+  "#c2410c",
+];
+
+export function PieChartCard({ data, title, description }: PieChartProps) {
+  const chartData = Object.entries(data).map(([key, value], index) => ({
+    name: key,
+    value,
+    fill: COLORS[index % COLORS.length],
+  }));
+
+  const total = chartData.reduce((sum, item) => sum + item.value, 0);
+
+  const convertToPercentage = (value: number) => {
+    return `${((value / total) * 100).toFixed(1)}%`;
+  };
+
+  const renderCustomLabel = ({ value }: { value: number }) => {
+    return `${convertToPercentage(value)}`;
+  };
+
+  const chartConfig = chartData.reduce((acc, { name }) => {
+    acc[name] = { label: name };
+    return acc;
+  }, {} as Record<string, { label: string }>);
+
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-4">
+        <CardTitle className="text-2xl">{title}</CardTitle>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square pb-0 [&_.recharts-pie-label-text]:fill-foreground [&_.recharts-pie-label-text]:text-xs [&_.recharts-legend-item-text]:text-base"
+        >
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart>
+              <Pie
+                data={chartData}
+                dataKey="value"
+                nameKey="name"
+                label={renderCustomLabel}
+                outerRadius={200}
+              />
+              <ChartTooltip content={<ChartTooltipContent />} />
+              <ChartLegend content={<ChartLegendContent nameKey="name" />} />
+              {/* <Legend className="text-xs mt-2" /> */}
+            </PieChart>
+          </ResponsiveContainer>
+        </ChartContainer>
+      </CardContent>
+      {description && (
+        <CardFooter className="flex-col gap-2 text-sm mt-4">
+          <div className="leading-none text-muted-foreground">
+            {description}
+          </div>
+        </CardFooter>
+      )}
+    </Card>
+  );
+}
